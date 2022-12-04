8a4557547f2e832d41e01b048cbfc5a84cabf983
Jake <zubarev.e.ev@gmail.com>
2022-12-05 01:23:23 +0300

    orika заменена на mapstruct

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "ru.acuma"
-version = "2.0.0"
+version = "2.0.1"
 
 repositories {
     mavenLocal()
@@ -15,7 +15,6 @@ repositories {
 
 catalog {
     versionCatalog {
-        version("shuffler-lib", project.version as String)
         version("spring-boot", "2.7.3")
         version("postgresql", "42.3.4")
         version("flyway", "8.5.10")
@@ -24,19 +23,19 @@ catalog {
         version("telegrambots", "6.0.1")
         version("junit", "5.8.2")
         version("mockito", "4.5.1")
-        version("orika", "2.2.7")
         version("gson", "2.9.0")
         version("lang3", "3.12.0")
         version("markdown", "1.3.2")
+        version("mapstruct", "1.5.3.Final")
+        version("mapstruct-lombok", "0.2.0")
 
         plugin("springframework", "org.springframework.boot").versionRef("spring-boot")
-
-        library("shuffler-lib", "ru.acuma", "shuffler-lib").versionRef("shuffler-lib")
 
         library("spring-starter", "org.springframework.boot", "spring-boot-starter").versionRef("spring-boot")
         library("spring-web", "org.springframework.boot", "spring-boot-starter-web").versionRef("spring-boot")
         library("spring-test", "org.springframework.boot", "spring-boot-starter-test").versionRef("spring-boot")
         library("spring-security", "org.springframework.boot", "spring-boot-starter-security").versionRef("spring-boot")
+        library("spring-aop", "org.springframework.boot", "spring-boot-starter-aop").versionRef("spring-boot")
 
         library("telegrambots", "org.telegram", "telegrambots").versionRef("telegrambots")
         library("telegrambotsextensions", "org.telegram", "telegrambotsextensions").versionRef("telegrambots")
@@ -51,13 +50,15 @@ catalog {
         library("junit", "org.junit.jupiter", "junit-jupiter-engine").versionRef("lombok")
         library("mockito", "org.mockito", "mockito-core").versionRef("mockito")
 
-        library("orika", "dev.akkinoc.spring.boot", "orika-spring-boot-starter").versionRef("orika")
+        library("mapstruct", "org.mapstruct", "mapstruct").versionRef("mapstruct")
+        library("mapstruct-processor", "org.mapstruct", "mapstruct-processor").versionRef("mapstruct")
+        library("mapstruct-lombok", "org.projectlombok", "lombok-mapstruct-binding").versionRef("mapstruct-lombok")
         library("gson", "com.google.code.gson", "gson").versionRef("gson")
         library("lang3", "org.apache.commons", "commons-lang3").versionRef("lang3")
         library("markdown", "com.github.Steppschuh", "Java-Markdown-Generator").versionRef("markdown")
 
         bundle("data", listOf("spring-jooq", "postgresql", "flyway"))
-        bundle("util", listOf("orika", "gson", "lang3", "markdown"))
+        bundle("util", listOf("gson", "lang3", "markdown"))
         bundle("lombok", listOf("lombok"))
         bundle("telegram", listOf("telegrambots", "telegrambotsextensions"))
         bundle("test", listOf("spring-test", "junit", "mockito"))
